6dbbe66e522c2b3b17a3c5cb83cb69caba4ed632
ihj9627 <wss9627@naver.com>
2022-04-18 17:36:31 +0900

    0418 profile

--- src/main/java/com/lol/java/profile/ProfileController.java
+++ src/main/java/com/lol/java/profile/ProfileController.java
@@ -2,6 +2,7 @@ package com.lol.java.profile;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 // 회원정보 페이지
@@ -16,4 +17,6 @@ public class ProfileController {
 	@RequestMapping("/profile.do")
 	public void profile() {
 	}
+	
+	
 }
