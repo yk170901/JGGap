130c901f08bef21eb202cbbd19c085c7a56e25af
Choble <97100708+Choble@users.noreply.github.com>
2022-04-28 17:41:49 +0900

    cy v10
    시네마틱 수정
    로그인 실패시 컨트롤러 수정

--- src/main/java/com/lol/java/user/UserController.java
+++ src/main/java/com/lol/java/user/UserController.java
@@ -9,6 +9,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.method.annotation.SessionAttributesHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 // 로그인페이지(메인) 및 회원가입페이지
@@ -26,7 +27,7 @@ public class UserController {
 		String encryPassword = UserSHA.encrypt(vo.getUser_pwd());
 		vo.setUser_pwd(encryPassword);
 		userService.sign_up(vo);
-
+		System.out.println(encryPassword);
 		return "redirect:/login.jsp";
 	}
 
@@ -38,18 +39,18 @@ public class UserController {
 		vo.setUser_pwd(encryPassword);
 		UserVO result = userService.idCheck_Login(vo);
 		System.out.println("login 컨트롤러 호출");
-
+		
 		if (result == null) {
-
-			System.out.println("존재하지 않는 아이디");
-			return "redirect:/login.jsp";
+			
+			System.out.println("존재하지 않는 아이디 또는 아이디 비밀번호가 틀렸습니다");
+			return "user/join";
 		} else if (result.getBan() == 1) {
 
 			System.out.println("정지당한 회원");
-			return "redirect:/login.jsp";
+			return "user/ban";
 		} else {
-
 			session.setAttribute("user_no", result.getUser_no());
+			
 			return "redirect:/board_list/board_list.do";
 		}
 	}
@@ -64,7 +65,7 @@ public class UserController {
 		return userService.idCheck(user_id);
 	}
 	
-	// id 중복 체크 컨트롤러
+	// 서머너id 중복 체크 컨트롤러
 	@RequestMapping(value = "/summoner_Check.do", method = RequestMethod.POST)
 	@ResponseBody
 	public String summoner_idCheck(String summoner_id) {
--- src/main/webapp/WEB-INF/views/user/join.jsp
+++ src/main/webapp/WEB-INF/views/user/join.jsp
@@ -5,8 +5,16 @@
 <head>
 <meta charset="UTF-8">
 <title>Insert title here</title>
+<script src="https://cdn.jsdelivr.net/npm/sweetalert2@11"></script>
 </head>
 <body>
-입력 완료
+<script type="text/javascript">
+Swal.fire({
+	icon: 'error',
+	title: '로그인 실패',
+	text: '아이디가 존재하지 않거나, 아이디 또는 비밀번호가 틀렸습니다.',   }).then(function(){
+	location.href='../../login.jsp';
+	})
+</script>
 </body>
 </html>
