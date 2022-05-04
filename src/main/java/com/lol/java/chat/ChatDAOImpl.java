6a71c1276e6ef00084cb73905b24e1f91de2ac7c
DongSoong2 <99469599+DongSoong2@users.noreply.github.com>
2022-05-04 14:09:21 +0900

    5월 4일 동성
    5월 4일 동성

--- src/main/java/com/lol/java/chat/ChatDAOImpl.java
+++ src/main/java/com/lol/java/chat/ChatDAOImpl.java
@@ -1,5 +1,7 @@
 package com.lol.java.chat;
 
+import java.util.List;
+
 import org.mybatis.spring.SqlSessionTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,11 @@ import org.springframework.stereotype.Repository;
 public class ChatDAOImpl implements ChatDAO{
 
 	@Autowired
-	private SqlSessionTemplate mybatis;	
+	private SqlSessionTemplate mybatis;
+
+	@Override
+	public List<ChatVO> user_info(ChatVO vo) {
+		return mybatis.selectList("ChatDAO.user_info", vo);
+	}	
 	
 }
--- src/main/java/com/lol/java/chat/ChatVO.java
+++ src/main/java/com/lol/java/chat/ChatVO.java
@@ -5,6 +5,6 @@ import lombok.Data;
 @Data
 public class ChatVO {
 
-	
-	
+	private String profile_icon;
+	private String summoner_id;
 }
