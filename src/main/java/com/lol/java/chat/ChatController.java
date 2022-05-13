ca3ac2c5db4744d96cfc23f68f9cad7800862719
DongSoong2 <99469599+DongSoong2@users.noreply.github.com>
2022-05-13 19:43:53 +0900

    5월 13일 오후7시 43분
    5월 13일 오후7시 43분

--- src/main/java/com/lol/java/chat/ChatController.java
+++ src/main/java/com/lol/java/chat/ChatController.java
@@ -16,6 +16,7 @@ public class ChatController {
 	@Autowired
 	private ChatService chatService;
 	
+	// 채팅방 입장시 새로운 세션 정보 등록
 	@RequestMapping("/chat.do")
 	public String chat(HttpSession session, Model model, HttpServletRequest request, ChatVO vo){
 		session.setAttribute("user_no", request.getParameter("user_no"));
@@ -34,6 +35,7 @@ public class ChatController {
 	}
 	
 	
+	// 아이언, 브론즈, 실버 티어방 입장
 	@RequestMapping("/chatBronze.do")
 	public String chatBronze(HttpSession session, Model model, HttpServletRequest request, ChatVO vo) {
 		session.getAttribute("user_no");
@@ -56,6 +58,7 @@ public class ChatController {
 		}
 	}
 	
+	// 골드, 플래티넘, 다이아몬드 티어방 입장
 	@RequestMapping("/chatGold.do")
 	public String chatGold(HttpSession session, Model model, HttpServletRequest request, ChatVO vo) {
 		session.getAttribute("user_no");
@@ -78,7 +81,7 @@ public class ChatController {
 		}
 	}
 	
-	
+	// 마스터, 그랜드마스터, 챌린저 티어방 입장
 	@RequestMapping("/chatMaster.do")
 	public String chatMaster(HttpSession session, Model model, HttpServletRequest request, ChatVO vo){
 		session.getAttribute("user_no");
@@ -101,7 +104,7 @@ public class ChatController {
 		}
 	}
 	
-	
+	// 언랭크 티어 채팅방 입장
 	@RequestMapping("/chatUnrank.do")
 	public String chatUnrank(HttpSession session, Model model, HttpServletRequest request, ChatVO vo) {
 		session.getAttribute("user_no");
